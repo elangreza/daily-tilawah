b926cb26b51cc8aacfca67af28bb2519417f36ef
Muhammad Afan Aljafar <muhammadafanaljafar@gmail.com>
2025-05-20 23:42:19 +0700

    adding progress donut

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,4 +1,10 @@
 import "./App.css";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import RadialSeparators from "./RadialSeparators";
 
 function App() {
   return (
@@ -10,6 +16,26 @@ function App() {
       <div className="btn-wrapper">
         <button className="btn-addProgres">Add Progress</button>
         <button className="btn-deleteProgress">Delete Progress</button>
+      </div>
+      <div className="progressDonut">
+        <CircularProgressbarWithChildren
+          value={80}
+          text={`${80}%`}
+          strokeWidth={10}
+          styles={buildStyles({
+            strokeLinecap: "butt",
+          })}
+        >
+          <RadialSeparators
+            count={12}
+            style={{
+              background: "#fff",
+              width: "2px",
+              // This needs to be equal to props.strokeWidth
+              height: `${10}%`,
+            }}
+          />
+        </CircularProgressbarWithChildren>
       </div>
     </div>
   );
